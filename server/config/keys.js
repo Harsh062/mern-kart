8e9199121706282794f3e8ec66a3d9c387c2bfb3
Harsh Gandhi <gandhiharsh3@gmail.com>
2018-07-20 08:05:40 +0530

    Delete keys.js
    Configuration files not be committed

--- server/config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    googleAuth: {
-        clientID: '48962189184-8ncamprnk22jfp3peh6dokd8ool5dljk.apps.googleusercontent.com',
-        clientSecret: '26XpvJDYF-NF-NWxdta8dy8l',
-        callbackURL: 'http://localhost:5000/auth/google/callback'
-    }
-}
